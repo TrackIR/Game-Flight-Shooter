ca1f98ccfa352195fdcd53b5684384e03b2f704e
SamuelCardwell <135365257+SamuelCardwell@users.noreply.github.com>
2026-01-11 17:39:00 -0800

    Removed old files fixed edge case where ship spawned on an asteroid

--- Assets/Spaceship/SpaceshipDamage.cs
+++ Assets/Spaceship/SpaceshipDamage.cs
@@ -9,23 +9,23 @@ public class SpaceshipDamage : MonoBehaviour
     //cooldown timer
     public float damageCooldown = 1f;
     private float lastDamageTime = 0f;
-
+    private float startTime;
     public GameObject gameOverMenu;
 
     // private SpaceshipMovement spaceshipMovement; 
     // private SpaceshipShoot spaceshipShoot;
 
-    // private void Start()
-    // {
-    //     spaceshipMovement = GetComponent<SpaceshipMovement>();
-    // }
+    private void Start()
+    {
+        startTime = Time.time;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Asteroid"))
         {
             // Only take damage if enough time has passed
-            if (Time.time - lastDamageTime >= damageCooldown)
+            if (Time.time - startTime - lastDamageTime >= damageCooldown)
             {
                 playerHealth -= 1;
                 lastDamageTime = Time.time;  // reset cooldown
@@ -40,19 +40,8 @@ public class SpaceshipDamage : MonoBehaviour
 
         if (playerHealth == 0)
         {
-            // GameOverUI ui = FindAnyObjectByType<GameOverUI>();
-            // if (ui != null)
-            // {
-                // ui.Show();
-                Time.timeScale = 0f;
-                // spaceshipMovement.OnDisable();
-                // // SpaceshipShoot.OnDestroy();
-                gameOverMenu.SetActive(true);
-            // }
-            // else
-            // {
-                // Debug.LogError("GameOverUI not found in scene!");
-            // }
+            Time.timeScale = 0f;
+            gameOverMenu.SetActive(true);
         }
     }
 }
